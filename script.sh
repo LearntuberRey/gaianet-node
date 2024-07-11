c0c4caf9f1d7676f3f30346c636842bfe6650f95
Zenith <161211651+dxzenith@users.noreply.github.com>
2024-07-11 11:00:34 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,8 +29,6 @@ echo
 
 sleep 3
 
-screen -S Gaianet
-
 echo -e "${bold_purple}Executing gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen-1.5-0.5b-chat/config.json${reset}"
 echo
 gaianet init --config https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen-1.5-0.5b-chat/config.json
